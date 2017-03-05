1dce481624c14103c646115db3176d9aac1f5f2b
victor.h.s.reis@gmail.com <victor.h.s.reis@gmail.com>
2017-03-05 18:21:51 -0300

    Inicio do Js

--- js/estacionar.js
+++ js/estacionar.js
@@ -1,1 +1,18 @@
-document.getElementById('formulario')
+document.getElementById('formulario').addEventListener('submit', cadVeiculo);
+
+function cadVeiculo(e) {
+  var modelo =  document.getElementById('modelo').value;
+  var placa = document.getElementById('placa').value;
+  var time = new Date();
+
+  carro = {
+    modelo: modelo,
+    palca: placa,
+    hora: time.getHours(),
+    min: time.getMinutes()
+  }
+  console.log(carro)
+
+  e.preventDefault();
+
+}
